100b64e16c0332311bae8f52fc20b8dc8c15ed06
Lucas henrique <silvaLUCAS444lucasLUCAS444@outlook.com>
2025-05-30 15:08:39 -0300

    ajustando vizualição grafico pizza

--- lib/axiosConfig.ts
+++ lib/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3003';
 
 const api = axios.create({
   baseURL: BASE_URL,
